174dab39d33c72eeac9ba9dca84ee5adac546438
Dyan8386 <phamdinhngancc151@gmail.com>
2025-07-02 13:33:52 +0700

    Update

---
+++ CozyCare.IdentityService/Controllers/Infrastructure/CatalogUnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using CozyCare.CatalogService.Domain.Entities;
+using CozyCare.CatalogService.Infrastructure.DBContext;
+using CozyCare.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace CozyCare.CatalogService.Infrastructure
+{
+    public class CatalogUnitOfWork : ICatalogUnitOfWork
+    {
+        private readonly CozyCareCatalogDbContext _context;
+
+        public CatalogUnitOfWork(CozyCareCatalogDbContext context)
+        {
+            _context = context;
+            Categories = new GenericRepository<Category>(_context);
+            Services = new GenericRepository<Service>(_context);
+            ServiceDetails = new GenericRepository<ServiceDetail>(_context);
+        }
+
+        public IGenericRepository<Category> Categories { get; }
+        public IGenericRepository<Service> Services { get; }
+        public IGenericRepository<ServiceDetail> ServiceDetails { get; }
+
+        public async Task<int> SaveChangesAsync() => await _context.SaveChangesAsync();
+
+        public void Dispose() => _context.Dispose();
+    }
+
+}
