53afc71bbfb5f98c0bc57bac265bb1f4b840eb3a
Dyan8386 <phamdinhngancc151@gmail.com>
2025-07-07 01:18:43 +0700

    Authentication(In Progressing)

--- CozyCare.IdentityService/Infrastructure/Repositories/IAuthenticationRepository.cs
+++ CozyCare.IdentityService/Infrastructure/Repositories/IAuthenticationRepository.cs
@@ -6,6 +6,5 @@ namespace CozyCare.IdentityService.Infrastructure.Repositories
     public interface IAuthenticationRepository : IGenericRepository<Account>
     {
         Task<Account?> AuthenticateAsync(string input, string password);
-
     }
 }
