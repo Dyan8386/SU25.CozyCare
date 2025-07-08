65fa7ab239a40c4476833585e6cf5e94a053956b
Dyan8386 <phamdinhngancc151@gmail.com>
2025-07-08 16:23:14 +0700

    Update profile

--- CozyCare.CatalogService/Application/Profiles/CatalogMappingProfile.cs
+++ CozyCare.CatalogService/Application/Profiles/CatalogMappingProfile.cs
@@ -14,32 +14,86 @@ namespace CozyCare.CatalogService.Application.Profiles
             CreateMap<Category, CategoryDto>().ReverseMap();
             CreateMap<CreateCategoryDto, Category>();
             CreateMap<UpdateCategoryDto, Category>()
-                .ForAllMembers(opts =>
-                    opts.Condition((src, dest, srcMember) =>
-                        srcMember != null &&
-                        (!ProfileHelper.IsNumericType(srcMember.GetType()) || !srcMember.Equals(0))
-                    )
-                );
+                // Map tất cả member nhưng với điều kiện chung cho số = 0 và datetime = default
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>
+                {
+                    if (srcMember == null)
+                        return false;
+
+                    // Nếu là decimal hoặc số nguyên, chỉ map khi khác 0
+                    if (ProfileHelper.IsNumericType(srcMember.GetType()) && srcMember.Equals(0))
+                        return false;
+
+                    var srcType = Nullable.GetUnderlyingType(srcMember.GetType()) ?? srcMember.GetType();
+                    if (ProfileHelper.IsNumericType(srcType))
+                    {
+                        var defaultValue = Activator.CreateInstance(srcType)!; // ví dụ default(decimal) = 0m
+                        if (srcMember.Equals(defaultValue))
+                            return false;
+                    }
+
+                    // Nếu là DateTime, chỉ map khi khác default(DateTime)
+                    if (srcMember is DateTime dt && dt == default(DateTime))
+                        return false;
+
+                    return true;
+                }));
             // Service Mappings
             CreateMap<Service, ServiceDto>().ReverseMap();
             CreateMap<CreateServiceDto, Service>();
             CreateMap<UpdateServiceDto, Service>()
-               .ForAllMembers(opts =>
-                    opts.Condition((src, dest, srcMember) =>
-                        srcMember != null &&
-                        (!ProfileHelper.IsNumericType(srcMember.GetType()) || !srcMember.Equals(0))
-                    )
-                );
+                // Map tất cả member nhưng với điều kiện chung cho số = 0 và datetime = default
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>
+                {
+                    if (srcMember == null)
+                        return false;
+
+                    // Nếu là decimal hoặc số nguyên, chỉ map khi khác 0
+                    if (ProfileHelper.IsNumericType(srcMember.GetType()) && srcMember.Equals(0))
+                        return false;
+
+                    var srcType = Nullable.GetUnderlyingType(srcMember.GetType()) ?? srcMember.GetType();
+                    if (ProfileHelper.IsNumericType(srcType))
+                    {
+                        var defaultValue = Activator.CreateInstance(srcType)!; // ví dụ default(decimal) = 0m
+                        if (srcMember.Equals(defaultValue))
+                            return false;
+                    }
+
+                    // Nếu là DateTime, chỉ map khi khác default(DateTime)
+                    if (srcMember is DateTime dt && dt == default(DateTime))
+                        return false;
+
+                    return true;
+                }));
             // ServiceDetail Mappings
             CreateMap<ServiceDetail, ServiceDetailDto>().ReverseMap();
             CreateMap<CreateServiceDetailDto, ServiceDetail>();
             CreateMap<UpdateServiceDetailDto, ServiceDetail>()
-                .ForAllMembers(opts =>
-                    opts.Condition((src, dest, srcMember) =>
-                        srcMember != null &&
-                        (!ProfileHelper.IsNumericType(srcMember.GetType()) || !srcMember.Equals(0))
-                    )
-                );
+                // Map tất cả member nhưng với điều kiện chung cho số = 0 và datetime = default
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>
+                {
+                    if (srcMember == null)
+                        return false;
+
+                    // Nếu là decimal hoặc số nguyên, chỉ map khi khác 0
+                    if (ProfileHelper.IsNumericType(srcMember.GetType()) && srcMember.Equals(0))
+                        return false;
+
+                    var srcType = Nullable.GetUnderlyingType(srcMember.GetType()) ?? srcMember.GetType();
+                    if (ProfileHelper.IsNumericType(srcType))
+                    {
+                        var defaultValue = Activator.CreateInstance(srcType)!; // ví dụ default(decimal) = 0m
+                        if (srcMember.Equals(defaultValue))
+                            return false;
+                    }
+
+                    // Nếu là DateTime, chỉ map khi khác default(DateTime)
+                    if (srcMember is DateTime dt && dt == default(DateTime))
+                        return false;
+
+                    return true;
+                }));
         }
     }
 }
