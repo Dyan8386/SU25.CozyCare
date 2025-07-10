434fe99c0efed68f430e5a8e9c76ab00cd216054
TwightNight <phamcuongtran222@gmail.com>
2025-07-11 02:06:59 +0700

    add BackgroundService: BookingExpiryService

--- CozyCare.BookingService/Program.cs
+++ CozyCare.BookingService/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CozyCare.BookingService;
+using CozyCare.BookingService.Applications.Externals;
 using CozyCare.BookingService.Infrastructure.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
@@ -50,6 +51,9 @@ builder.Services.AddCors(options =>
 			   .AllowAnyHeader();
 	});
 });
+
+// Đăng ký hosted service
+builder.Services.AddHostedService<BookingExpiryService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
